00e50946e29fc21f99585c144f9864e6ad5af4ef
Chris Howie <me@chrishowie.com>
2013-07-17 22:48:54 -0900

    Add EnumerateArray() extension for tables

--- Eluant/LuaValueExtensions.cs
+++ Eluant/LuaValueExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Eluant
 {
@@ -7,6 +8,40 @@ namespace Eluant
         public static bool IsNil(this LuaValue self)
         {
             return self == null || self == LuaNil.Instance;
+        }
+
+        public static IEnumerable<LuaValue> EnumerateArray(this LuaTable self)
+        {
+            if (self == null) { throw new ArgumentNullException("self"); }
+
+            return CreateEnumerateArrayEnumerable(self);
+        }
+
+        private static IEnumerable<LuaValue> CreateEnumerateArrayEnumerable(LuaTable self)
+        {
+            // By convention, the 'n' field refers to the array length, if present.
+            using (var n = self["n"]) {
+                var num = n as LuaNumber;
+                if (num != null) {
+                    var length = (int)num.Value;
+
+                    for (int i = 1; i <= length; ++i) {
+                        yield return self[i];
+                    }
+
+                    yield break;
+                }
+            }
+
+            // If no 'n' then stop at the first nil element.
+            for (int i = 1; ; ++i) {
+                var value = self[i];
+                if (value.IsNil()) {
+                    yield break;
+                }
+
+                yield return value;
+            }
         }
     }
 }
